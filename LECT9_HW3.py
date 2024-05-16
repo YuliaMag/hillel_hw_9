fdc7c2d0645167c5cc29e1af61fd2b3339eda3cc
Yulia Magonya <ymagonia@gmail.com>
2024-05-16 23:10:57 +0300

    schedl corr

--- LECT9_HW3.py
+++ LECT9_HW3.py
@@ -7,8 +7,10 @@ timestamp = 1715703300
 dt_object = datetime.fromtimestamp(timestamp)
 evtime = dt_object.strftime("%H:%M")
 
+count = 0
 
 for d in range(0, (edate - sdate).days + 1):
     if (sdate + timedelta(days=d)).weekday() == 0 or (sdate + timedelta(days=d)).weekday() == 3:
-        print((sdate + timedelta(days=d)).strftime(f"Lecture {""}: %d %h %Y {evtime}"))
-
+        if count in range(0, (edate - sdate).days + 1):
+            count += 1
+        print((sdate + timedelta(days=d)).strftime(f"Lecture {count.__str__().rjust(2)}: %d %h %Y {evtime}"))
