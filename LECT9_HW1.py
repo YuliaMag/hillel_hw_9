35b341f07cbbebb8b75f9fd9905566a46b6ce7c0
Yulia Magonya <ymagonia@gmail.com>
2024-05-16 19:33:34 +0300

    em_val corr

--- LECT9_HW1.py
+++ LECT9_HW1.py
@@ -1,25 +1,30 @@
 import re
 
-rex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+rex = r"[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Z|a-z|0-9]{2,7}"
 
 
-def val_email(email=input("Email Address: ")):
+def val_email(email):
     if re.fullmatch(rex, email):
         print(True)
     else:
         print(False)
 
 
-val_email()
+email = input("Email Address: ")
+
+val_email(email)
 
 # ================================================ #
 # TC "True":
 # test@test.com
+# t3st@t1st.c0m -- for "symbols except '@' and '.' should be letters and decimal digits"
+# 111@qqq.77 -- for "symbols except '@' and '.' should be letters and decimal digits"
 
 # TCs "False":
 # test.test@com
 # test@@test@com
 # test.test.com
+# test.test..com
 # testtestcom
 # ^%&!#$%^&*()_+><?
 # ^%&test
