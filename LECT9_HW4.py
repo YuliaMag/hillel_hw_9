298dd02002db8d18cf855feb310fccdf9fb1b298
Yulia Magonya <ymagonia@gmail.com>
2024-05-17 21:56:56 +0300

    did

---
+++ LECT9_HW4.py
@@ -0,0 +1,22 @@
+
+# HW 4.1 (using regular expressions)
+import re
+with open("wiki_page.txt", "r") as f:
+    text = f.read()
+res = re.findall(r"\d{3}.*?\D\d{3}", text)
+print(res)
+
+# =============================================== #
+
+with open("wiki_page.txt", "r") as f:
+    text = f.read()
+
+res = []
+
+for line in text.split(" "):
+    if "href=" in line and line.startswith("href=") and line.endswith("\""):
+        ln = line.removeprefix("href=\"").removesuffix("\"")
+        res.append(ln)
+
+print(res)
+
