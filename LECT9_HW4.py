847e317e2780b7852ddfcd911ef75ff1cef1bd80
Yulia Magonya <ymagonia@gmail.com>
2024-05-20 01:36:12 +0300

    --

--- LECT9_HW4.py
+++ LECT9_HW4.py
@@ -3,21 +3,26 @@
 import re
 with open("wiki_page.txt", "r") as f:
     text = f.read()
-res = re.findall(r"\d{3}.*?\D\d{3}", text)
+
+res = re.findall(r"<a.href=\"(.*?)\"", text)
 print(res)
 
 # =============================================== #
 
 # HW 4.2 (using string methods)
-with open("wiki_page.txt", "r") as f:
-    text = f.read()
 
 res = []
+tag_a = "<a"
 
-for line in text.split(" "):
-    if "href=" in line and line.startswith("href=") and line.endswith("\""):
-        ln = line.removeprefix("href=\"").removesuffix("\"")
-        res.append(ln)
+with open("wiki_page.txt") as f:
+    for line in f:
+        if tag_a in line:
+            start_tag_a_index = line.index(tag_a)
+            start_href_attr_index = line.index("href=\"", start_tag_a_index)
+            end_href_attr_index = line.index("\"", start_href_attr_index + len("href=\""))
+            href_value = line[(start_href_attr_index + len("href=\"")): end_href_attr_index]
+            res.append(href_value)
 
 print(res)
 
+# line_n = str([i for i in range(len(line)) if line.startswith(tag_a, i)])
