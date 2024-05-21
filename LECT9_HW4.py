a8aea8ddfc6ca1cf2f339b680e8fe54296e17bd1
Yulia Magonya <ymagonia@gmail.com>
2024-05-22 01:55:09 +0300

    qq

--- LECT9_HW4.py
+++ LECT9_HW4.py
@@ -4,25 +4,31 @@ import re
 with open("wiki_page.txt", "r") as f:
     text = f.read()
 
-res = re.findall(r"<a.href=\"(.*?)\"", text)
+res = re.findall(r"<a.*?href=\"(.*?)\"", text)
 print(res)
 
 # =============================================== #
 
 # HW 4.2 (using string methods)
 
-res = []
-tag_a = "<a"
-
 with open("wiki_page.txt") as f:
     for line in f:
-        if tag_a in line:
-            start_tag_a_index = line.index(tag_a)
-            start_href_attr_index = line.index("href=\"", start_tag_a_index)
-            end_href_attr_index = line.index("\"", start_href_attr_index + len("href=\""))
-            href_value = line[(start_href_attr_index + len("href=\"")): end_href_attr_index]
-            res.append(href_value)
+        res = []
+        start_index = 0
+        while start_index != -1:
+            start_index = line.find("<a", start_index)
+            if start_index == -1:
+                break
+            else:
+                start_index = line.find("href=\"", start_index) + len("href=\"")
+                end_index = line.find("\" ", start_index)
+                href_value = line[start_index: end_index]
+                res.append(href_value)
+                print(res, end="")
 
-print(res)
 
-# line_n = str([i for i in range(len(line)) if line.startswith(tag_a, i)])
+
+
+
+
+
