dc923ef1a0c251681067091f1058792fece0b5c5
Yulia Magonya <ymagonia@gmail.com>
2024-05-22 18:17:07 +0300

    000000000000--0000000000000

--- LECT9_HW4.py
+++ LECT9_HW4.py
@@ -12,20 +12,20 @@ print(res)
 # HW 4.2 (using string methods)
 
 with open("wiki_page.txt") as f:
-    for line in f:
-        ln = f.read()
-        res = []
-        start_index = 0
-        while start_index != -1:
-            start_index = ln.find("<a ", start_index)
-            if start_index == -1:
-                break
-            else:
-                start_index = ln.find("href=\"", start_index) + len("href=\"")
-                end_index = ln.find("\" ", start_index)
-                href_value = ln[start_index: end_index]
-                res.append(href_value)
-        print(res)
+    # for line in f:
+    ln = f.read()
+    res = []
+    start_index = 0
+    while start_index != -1:
+        start_index = ln.find("<a ", start_index)
+        if start_index == -1:
+            break
+        else:
+            start_index = ln.find("href=\"", start_index) + len("href=\"")
+            end_index = ln.find("\" ", start_index)
+            href_value = ln[start_index: end_index]
+            res.append(href_value)
+    print(res)
 
 
 
